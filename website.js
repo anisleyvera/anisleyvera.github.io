5b7ae526be8df05e66fe147657b13681da0e4849
anisleyvera <40634526+anisleyvera@users.noreply.github.com>
2018-06-27 09:52:03 -0400

    Add files via upload

---
+++ website.js
@@ -0,0 +1,30 @@
+// .alert{
+//   padding: 20px;
+//   background-color: blue;
+//   color: white
+// }
+//
+// .closebtn {
+//     margin-left: 15px;
+//     color: white;
+//     font-weight: bold;
+//     float: right;
+//     font-size: 22px;
+//     line-height: 20px;
+//     cursor: pointer;
+//     transition: 0.3s;
+// }
+//
+// <body>
+//
+// <h2>Alert Messages</h2>
+//
+// <p>Click on the "x" symbol to close the alert message.</p>
+// <div class="alert">
+//   <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+//   <strong>Info!</strong> You have made it to my web page.
+// </div>
+//
+// </body>
+
+window.alert("Webpage reached")
